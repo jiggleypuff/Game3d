1600516af77299ca7eac8195329c9c9cf1639056
Луценко Никита <nlutsenko@diasoft.ru>
2020-04-09 15:43:38 +0300

    cleanup

--- core/src/ru/quarantine/escape/Player.java
+++ core/src/ru/quarantine/escape/Player.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.math.Vector3;
 public class Player {
     private Vector3 position;
     private float yaw = 0;
-    private float speed = 0.3f;
+    private float speed = 1.3f;
     private float rotSpeed = 2f;
 
     public Player(){
-        position = new Vector3(0,1.5f, 0);
+        position = new Vector3(0,0, 0);
     }
 
     public Vector3 getPos(){
@@ -48,6 +48,20 @@ public class Player {
         Vector3 move = Vector3.Zero.cpy();
         move.x = (float) Math.sin(Math.toRadians(yaw + 90));
         move.z = (float) -Math.cos(Math.toRadians(yaw + 90));
+        move.scl(speed);
+        position.add(move);
+    }
+
+    public void moveUp(){
+        Vector3 move = Vector3.Zero.cpy();
+        move.y = 1;
+        move.scl(speed);
+        position.add(move);
+    }
+
+    public void moveDown(){
+        Vector3 move = Vector3.Zero.cpy();
+        move.y = -1;
         move.scl(speed);
         position.add(move);
     }
